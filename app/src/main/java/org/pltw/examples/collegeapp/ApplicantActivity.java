ffe095466b1317d8201f1a5b881fd0076004d022
Derionicle <nmerz@icloud.com>
2015-10-27 18:33:48 -0400

    1.2.5 About Finished

--- app/src/main/java/org/pltw/examples/collegeapp/ApplicantActivity.java
+++ app/src/main/java/org/pltw/examples/collegeapp/ApplicantActivity.java
@@ -3,23 +3,64 @@ package org.pltw.examples.collegeapp;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ApplicantActivity extends FragmentActivity {
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    private void selectItem(int position) {
+        System.out.println(position);
+        Fragment fragment = null;
+        if (position == 0) {
+            fragment = new GuardianFragment();
+        } else if (position == 1){
+            fragment = new Viewing();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
     private static final String TAG = "ApplicantActivity";
+    private String[] screens = {"Guardian", "View Applicant Info"};
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_member);
-        System.out.println("CollegeApp Created");
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, screens));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        System.out.println(screens[0]+"CollegeApp Created");
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = null;
-        if (fragment == null){
-            fragment = new GuardianFragment();
-            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-        }
+        Fragment fragment = new GuardianFragment();
+        fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
 
     }
 
